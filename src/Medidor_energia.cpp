333d9e268de3b32665f38b1fd0a112c2fe0ed9e9
rudsomlima <rudsomlima@gmail.com>
2017-07-31 14:06:46 -0300

    Node-red em um único envio

--- src/Medidor_energia.cpp
+++ src/Medidor_energia.cpp
@@ -10,14 +10,14 @@ extern "C"{
 #define wifi_ssid "PAPAYA"
 #define wifi_password "papaya2014"
 
-#define mqtt_server "192.168.0.47"
+#define mqtt_server "192.168.0.60"
 #define mqtt_user "general"
 #define mqtt_password "smolder79"
 
 #define topico_medidor "medidor"
-#define topico_lum "lum"
-#define topico_set_lum "lum_setada"
+#define topico_set_lum "set_lum"
 #define topico_input_lum "input_lum"
+#define topico_pulso_alto "pulso_alto_max"
 
 WiFiClient espClient;
 void callback(char* topic, byte* payload, unsigned int length);
@@ -32,11 +32,16 @@ os_timer_t mTimer;
 int contador, leituras, cont_pulso;
 bool led_pulso, led_pulso_ant=0, pisca;
 int opt_off = 10;  //inserir a leitura do opto sensor quando o led desligado
+int pulso_alto, pulso_alto_max; //valor da medicao do led na piscada
+String envio;
 
 //Nunca execute nada na interrupcao, apenas setar flags!
 void tCallback(void *tCall){
-    if(analogRead(A0)>opt_off) led_pulso = 1;  //testa o estado do led atraves do leitor optico
+    pulso_alto = analogRead(A0);
+    if(pulso_alto>opt_off) led_pulso = 1;  //testa o estado do led atraves do leitor optico
     else led_pulso = 0;
+
+    if(pulso_alto>pulso_alto_max)  pulso_alto_max = pulso_alto; //pega o valor maximo do pulso do led
 
     if(!led_pulso_ant & led_pulso) cont_pulso++; //se mudou de 0 pra 1, incrementa
     led_pulso_ant = led_pulso;
@@ -47,7 +52,7 @@ void tCallback(void *tCall){
 //FUNCAO DE EXECUCAO COM ESTOURO DE TIMER
 void usrInit(void){
     os_timer_setfn(&mTimer, tCallback, NULL);  //Chama a funcao tCallback a cada 10ms
-    os_timer_arm(&mTimer, 10, true);  //10
+    os_timer_arm(&mTimer, 10, true);  //10ms
 }
 
 #define LED_AZUL 2 // led azul na placa lolin nodemcu v3
@@ -97,15 +102,23 @@ void setup_wifi() {
 void callback(char* topic, byte* payload, unsigned int length) {
   String msg = "";
   Serial.print("Mensagem recebida [");
-  Serial.print(topic);
+  String topico = String(topic);
+  Serial.print(topico);
   Serial.print("] ");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
     msg += (char)payload[i];
   }
   Serial.println();
-  if(topic==topico_input_lum) opt_off = msg.toInt();
-  Serial.println(opt_off);
+  if(topico==topico_input_lum) {
+    if(msg=="[]") cont_pulso=0; //se receber a msg "[]" zera o medidor KWh
+    else {
+      opt_off = msg.toInt();
+      Serial.print("opt_off setado: ");
+      Serial.println(opt_off);
+    }
+  }
+  // Serial.println(msg);
 }
 
 void reconnect() {
@@ -140,9 +153,12 @@ void loop()
   Serial.println(cont_pulso);
   Serial.print("leituras por segundo: ");
   Serial.println(contador);
-  valor_lum = analogRead(A0);
+  //valor_lum = analogRead(A0);
   Serial.print("valor_lum: ");
-  Serial.println(valor_lum);
+  Serial.println(pulso_alto);
+  Serial.print("pulso_alto_max: ");
+  Serial.println(pulso_alto_max);
+
   //Serial.println(leituras);
   //leituras++;
   contador=0;  //zera a contagem de interrupcoes por segundo q esta ocorrendo
@@ -153,6 +169,16 @@ void loop()
   digitalWrite(LED_AZUL, HIGH);
   delay(990);
   yield();
+  envio = String(cont_pulso) + "n" + String(pulso_alto) + "n" + String(pulso_alto_max);
+  // client.publish(topico_medidor, String(cont_pulso).c_str(), true); //publica a contagem de pulsos KWh
+  // yield();
+  client.publish(topico_set_lum, String(envio).c_str(), true); //publica a contagem de pulsos KWh
+  Serial.println(envio);
+  yield();
+  // client.publish(topico_input_lum, String(pulso_alto).c_str(), true); //publica a contagem de pulsos KWh
+  // yield();
+  // client.publish(topico_pulso_alto, String(pulso_alto_max).c_str(), true); //publica a contagem de pulsos KWh
+  // yield();
 
 
 
@@ -160,9 +186,10 @@ void loop()
 
   if (now - lastMsg > 10000) {   //executa a cada 10s
     lastMsg = now;
-    client.subscribe("input_lum");   //faz uma leitura no topico lum para receber msg
-    client.publish(topico_lum, String(valor_lum).c_str(), true); //publica a luminosidade do sensor
-    client.publish(topico_medidor, String(cont_pulso).c_str(), true); //publica a contagem de pulsos KWh
-    client.publish(topico_input_lum, String(opt_off).c_str(), true); //publica a contagem de pulsos KWh
+    client.subscribe("input_lum");   //faz uma leitura no topico lum para receber msgclient.publish(topico_lum, String(valor_lum).c_str(), true); //publica a luminosidade do sensor
+    yield();
+
+    pulso_alto_max=0; //reinicia o pulso_alto_max
+
   }
 }
